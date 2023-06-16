ba622179265108295c09c18f50b899358f4b3e24
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-16 15:59:43 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function that prints the assignment
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
