bcd96fd591f4c847ee009504eac6717a28559b53
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-15 17:50:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sentence asked for
+ * Return: 0 if done properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
