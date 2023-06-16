447082d2ac43d28c9c8ea9621320f31bd19161d9
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-15 18:29:21 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - prints something
+ * Return: 0 is executable, else non zero
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
