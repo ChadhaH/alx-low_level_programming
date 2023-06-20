debac319fec29eb951d54c2f54b422a075cf32df
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-20 12:21:10 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,10 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	
 	for (i = 0; i <= 9; i++)
 	{
 		int j;
 		char str[] = "abcdefghijklmnopqrstuvwxyz";
-		
 		for (j = 0; j < 26; j++)
 		{
 			_putchar(str[j]);
