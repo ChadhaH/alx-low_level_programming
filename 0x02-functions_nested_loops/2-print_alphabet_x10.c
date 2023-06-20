bc36fa420a29ed58549f517d4fb76ca47009bef0
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-20 12:17:32 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	for (i = 0; i < 9; i++)
+	{
+		int j;
+		char str[] = "abcdefghijklmnopqrstuvwxyz";
+		
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(str[j]);
+		}
+	_putchar('\n');
+	}
+}
