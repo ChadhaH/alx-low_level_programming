be3cc00f05288a9342676f11f08055956d34aca4
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-20 12:04:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * print_alphabet - the function that prints alphabet
+ */
+void print_alphabet(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
