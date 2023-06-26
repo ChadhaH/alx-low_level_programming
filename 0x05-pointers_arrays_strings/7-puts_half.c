f359c3240db03be0d6120f309b161b2cf0722b72
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-26 11:43:25 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int ln = 0;
+	int i;
+	
+	while (str[ln] != '\0')
+	{
+		ln++;
+		str++;
+	}
+	if (ln % 2 != 0)
+	{
+		for (i = (ln - 1) / 2; i < ln -1; i++)
+		{
+			_putchar(str[i + 1]);
+		}	
+	}
+	else if (ln % 2 = 0)
+	{
+		for (i = ln / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
