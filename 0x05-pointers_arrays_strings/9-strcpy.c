826dea386b2d6595a3e9580e92706db2b136d773
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-26 12:24:37 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function
+ * @dest: string
+ * @src: string
+ * Return: pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+        int ln, i;
+
+        ln = 0;
+
+        while (src[ln] != '\0')
+        {
+                ln++;
+        }
+
+        for (i = 0; i < ln; i++)
+		dest[i] = src [i];
+	dest[i] = '\0';
+
+	return (dest);
+}
