0a5e2916ed3c4f296083d30e9b226f9bdb3f2492
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-26 09:47:44 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values
+ * @a: first number
+ * @b: second number
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int trial
+	trial == *a;
+	*a == *b;
+	*b == trial;
+}
