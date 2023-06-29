9cc3cab0d6b9e939a46782afcb0800d1db767892
Chadha Hamed <chadha.hamed1@gmail.com>
2023-06-29 09:46:12 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: the difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	j = 0;
+
+	while (s1[j] != '\0' && s2[j] != '\0')
+	{
+		if (s1[j] != s2[j]
+				return (s1[j] - s2[j]);
+		j++;
+	}
+
+	return (0);
+}
