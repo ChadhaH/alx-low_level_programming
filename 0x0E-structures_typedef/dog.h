2ee9b9d89a934693eeaadecbd2d83e04ac357d58
ChadhaH <Chadha.hamed1@gmail.com>
2023-07-17 15:49:55 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic info
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
