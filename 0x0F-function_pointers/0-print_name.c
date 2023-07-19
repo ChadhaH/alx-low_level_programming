11d810a27a9525d72f697cda9351d71d2a92450b
ChadhaH <Chadha.hamed1@gmail.com>
2023-07-19 07:23:58 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
