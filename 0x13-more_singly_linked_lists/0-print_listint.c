02f7f1e486f8a57b6515d75c6af35ffb4bc57723
ChadhaH <chadha.hamed1@gmail.com>
2023-08-01 08:54:46 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,6 @@
  * @h: list
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
@@ -16,5 +15,6 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 	}
+
 	return (num);
 }
