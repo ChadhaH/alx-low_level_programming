0b6e7eb5ea1ddf2629b72e8d2fad990aaf5bbc2c
ChadhaH <chadha.hamed1@gmail.com>
2023-10-10 10:30:02 -0700

    all

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,3 +15,25 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (ht == NULL)
 		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
